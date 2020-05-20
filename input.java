f0c05266c84f19714894895bc4e2c52194c6b4b8
ShanbhagAdarsh <adushanu31@yahoo.com>
2020-05-20 18:39:20 +0530

    Java inputs

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class input
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+	        int n;
+		System.out.println("Enter the number");
+		n=sc.nextInt();
+		System.out.println("You have entered "+n);
+	}
+}
