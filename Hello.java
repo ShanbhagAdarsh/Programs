b38ff2c8644936ace23c66b09a5dcf29fb100d41
ShanbhagAdarsh <adushanu31@yahoo.com>
2020-05-20 23:11:50 +0530

    Hello world

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		System.out.println("Hello World!My firt Code....");
 		System.out.println("Welcome to JAVA");
 		System.out.println("Lets start Coding...");
 		System.out.println("How r u all.....?");
