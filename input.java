03a175955d436ff7bd0c9a720806c28e05501051
ShanbhagAdarsh <adushanu31@yahoo.com>
2020-05-20 19:07:05 +0530

    Java input file in feature-1

--- input.java
+++ input.java
@@ -4,9 +4,18 @@ class input
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-	        int num;
-		System.out.println("Enter the number");
-		num=sc.nextInt();
-		System.out.println("You have entered "+num);
+	        int num1,num2,choice;
+		System.out.println("Enter the first number");
+		num1=sc.nextInt();
+		System.out.println("Enter the second number");
+		num2=sc.nextInt();
+		System.out.println("ENTER 1 TO ADD \nENTER 2 TO SUBTRACT");
+		choice=sc.nextInt();
+		if(choice==1)
+			System.out.println("ADDITION IS "+(num1+num2));
+		else if(choice==2)
+			System.out.println("SUBTRACTION IS "+(num1-num2));
+		else
+			System.out.println("Invalid Input.....");
 	}
 }
