39978ec5184401a471f20889663c0201d59b5340
ShanbhagAdarsh <adushanu31@yahoo.com>
2020-11-01 10:51:17 +0530

    Counter class Version-1

---
+++ Salary_Package/Counter_Class.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary_Package
+{
+    class Counter_Class
+    {
+        public string cnt { get; set; }
+    }
+}
+
